efee4ce9496f721dac79213844d84195985c7f5b
SH Hridoy <155417546+hridoyrocks@users.noreply.github.com>
2025-05-14 11:31:43 +0600

    Update show.blade.php

--- resources/views/forms/public/show.blade.php
+++ resources/views/forms/public/show.blade.php
@@ -176,7 +176,7 @@
         <footer class="py-4">
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div class="text-center text-sm text-gray-500">
-                    © {{ date('Y') }} Form Management System
+                    © {{ date('Y') }} BIIC. Made with ❤️ Rocks
                 </div>
             </div>
         </footer>
